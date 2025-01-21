3f7dff2ed4c82493362cb1fed25873839923cda2
cho <inbrain144@gmail.com>
2025-01-21 23:51:14 +0900

    2025.01.21 DP 사각형 채우기 2
    - 차분하게 n=4까지 대입해서 점화식 잘 찾아냄

---
+++ Codetree_Practice/2025-01/FillSqueare2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class FillSqueare2 {
+    static int n;
+    static int[] memo;
+
+    public static int dp(int num){
+        if(num<0) return 0;
+        if(memo[num]!=0) return memo[num];
+        if(num<=2){
+            if(num==1) memo[num] = 1;
+            if(num==2) memo[num] = 3;
+        }
+        else{
+            memo[num] = (dp(num-1)+dp(num-2)*2)%10007;
+        }
+        return memo[num];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        memo = new int[n+1];
+        System.out.print(dp(n));
+    }
+}
