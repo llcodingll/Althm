61517bc03608b1b54eaeb10e6277c3ec05d244db
llcodingll <ll.o.llwhkr@gmail.com>
2025-01-22 14:56:20 +0900

    algo: 22th DigitTest1

--- 01.2025/DigitTest1.java
+++ 01.2025/DigitTest1.java
@@ -11,12 +11,14 @@ public class DigitTest1 {
 		 */
 
 		 int n = 5;
+		 int count = 0;
 		 for (int i = 0; i < n; i++) {
 			for (int j = 0; j < i; j++) {
 				System.out.print(" ");
 			}
 			for (int j = 0; j < n-i; j++) {
-				System.out.print("*");
+				System.out.printf("%-3d", count);
+				count++;
 			}
 			System.out.println();
 		 }
