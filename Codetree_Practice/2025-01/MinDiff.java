345d482039be6ffad3502cd7b909ac14173c62b3
cho <inbrain144@gmail.com>
2025-01-23 23:34:13 +0900

    2024.01.23 DP - 정수 사각형 차이의 최소 2
    - 너무너무 어려운 문제였음...
    - 내 방식대로 하니 에러가 나서 결국 풀이를 보고 구현했음.
    - dp[i][j][k] : (1,1)에서 (i,j)까지 이동한 경로 중 가장 작은 값을 k라 했을 때 경로 중 있었던 수들 중 가장 큰 값 중 가능한 최솟값 으로 전제를 하고 진행하면 되는 건데, 이걸 알고 풀어도 엄청 어렵더라...
    - 나중에 한 일주일 지나서 다시 풀어봐야겠음

--- Codetree_Practice/2025-01/MinDiff.java
+++ Codetree_Practice/2025-01/MinDiff.java
@@ -2,32 +2,36 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-class MinMax{
-    int min;
-    int max;
-
-    public MinMax(int min, int max){
-        this.min = min;
-        this.max = max;
-    }
-}
-
 public class MinDiff {
     static int n;
     static int[][] grid;
-    static MinMax[][] dp;
+    static int[][][] dp; // dp[i][j][k] : (1,1)에서 (i,j)까지 이동한 경로 중 가장 작은 값을 k라 했을 때 경로 중 있었던 수들 중 가장 큰 값 중 가능한 최솟값
+
+    static int answer = Integer.MAX_VALUE;
 
     public static void init(){
-        dp[0][0] = new MinMax(grid[0][0], grid[0][0]);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                for(int k = 0; k<100; k++){
+                    dp[i][j][k] = Integer.MAX_VALUE;
+                }
+            }
+        }
 
-        for(int i=1; i<n; i++){
-            dp[i][0] = new MinMax(grid[i][0], grid[i][0]);
-            dp[i][0].min = Math.min(dp[i-1][0].min, grid[i][0]);
-            dp[i][0].max = Math.max(dp[i-1][0].max, grid[i][0]);
+        dp[0][0][grid[0][0]] = grid[0][0];
 
-            dp[0][i] = new MinMax(grid[0][i], grid[0][i]);
-            dp[0][i].min = Math.min(dp[0][i-1].min, grid[0][i]);
-            dp[0][i].max = Math.max(dp[0][i-1].max, grid[0][i]);
+        for(int i = 1; i<n; i++){
+            for(int k = 1; k<100; k++){
+                int min = Math.min(k, grid[i][0]);
+                dp[i][0][min] = Math.min(dp[i][0][min], Math.max(dp[i-1][0][k],grid[i][0]));
+            }
+        }
+
+        for(int j = 1; j<n; j++){
+            for(int k = 1; k<100; k++){
+                int min = Math.min(k, grid[0][j]);
+                dp[0][j][min] = Math.min(dp[0][j][min], Math.max(dp[0][j-1][k],grid[0][j]));
+            }
         }
     }
 
@@ -37,7 +41,7 @@ public class MinDiff {
 
         n = Integer.parseInt(st.nextToken());
         grid = new int[n][n];
-        dp = new MinMax[n][n];
+        dp = new int[n][n][100];
         for(int i = 0 ; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0 ; j<n; j++){
@@ -47,23 +51,20 @@ public class MinDiff {
 
         init();
 
-        for(int i=1; i<n; i++){
-            for(int j=1; j<n; j++){
-                int tmpRightMin = Math.min(dp[i][j-1].min, grid[i][j]);
-                int tmpRightMax = Math.max(dp[i][j-1].max, grid[i][j]);
-                int tmpDownMin = Math.min(dp[i-1][j].min, grid[i][j]);
-                int tmpDownMax = Math.max(dp[i-1][j].max, grid[i][j]);
-
-                if(tmpRightMax-tmpRightMin > tmpDownMax-tmpDownMin)dp[i][j] = new MinMax(tmpDownMin, tmpDownMax);
-                else if(tmpRightMax-tmpRightMin == tmpDownMax-tmpDownMin){
-                    if(tmpRightMax>tmpDownMax) dp[i][j] = new MinMax(tmpDownMin, tmpDownMax);
-                    else dp[i][j] = new MinMax(tmpRightMin, tmpRightMax);
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<n; j++){
+                for(int k = 1; k<100; k++){
+                    int min = Math.min(k, grid[i][j]);
+                    dp[i][j][min] = Math.min(
+                            dp[i][j][min],
+                            Math.max(Math.min(dp[i-1][j][k],dp[i][j-1][k]), grid[i][j]));
                 }
-                else dp[i][j] = new MinMax(tmpRightMin, tmpRightMax);
-                //System.out.println(dp[i][j].min+", "+dp[i][j].max);
             }
         }
 
-        System.out.print(dp[n-1][n-1].max - dp[n-1][n-1].min);
+        for(int k = 0; k< 100; k++){
+            if(dp[n-1][n-1][k] != Integer.MAX_VALUE) answer = Math.min(answer, dp[n-1][n-1][k] - k);
+        }
+        System.out.print(answer);
     }
 }
