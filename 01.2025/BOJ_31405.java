230f38ea0a1167ae212c44e0424ea3d82a5a69c4
llcodingll <ll.o.llwhkr@gmail.com>
2025-01-27 21:52:41 +0900

    algo: 27th BufferedReader 첫 사용, 숫자합/문자파스합

---
+++ 01.2025/BOJ_31405.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_31405 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        String s = "";
+        s += a;
+        s += b;
+
+        System.out.println(a + b - c);
+        System.out.println(Integer.parseInt(s) - c);
+    }
+}
